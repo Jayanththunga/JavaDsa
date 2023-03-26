026646dca5ba7f40582abdb8be9e38f99da8aadb
Jayanththunga <jayanththunga27@gmail.com>
2023-03-26 14:43:07 +0530

    added

--- 5-LinkedList/Medium/reorderList.java
+++ 5-LinkedList/Medium/reorderList.java
@@ -27,4 +27,38 @@ public class reorderList {
         if (i == j)
             temp.val = arr.get(i);
     }
+
+    // without using ArrayList:
+
+    public void reorderList2(ListNode head) {
+        // finding midpoint:
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // reversing part 2:
+        ListNode prev = null, curr = slow.next;
+        slow.next = null;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        // merging both:
+        ListNode curr1 = head, curr2 = prev;
+        while (curr1 != null && curr2 != null) {
+            ListNode next1 = curr1.next;
+            ListNode next2 = curr2.next;
+
+            curr1.next = curr2;
+            curr2.next = next1;
+            curr1 = next1;
+            curr2 = next2;
+        }
+    }
+
 }
