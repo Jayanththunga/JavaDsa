7a6e2f04e6fb9620e82e10e0803f13efe8e34d0f
Jayanththunga <jayanththunga27@gmail.com>
2023-05-09 11:24:30 +0530

    added

---
+++ 5-LinkedList/Medium/rotateListright.java
@@ -0,0 +1,22 @@
+public class rotateListright {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null)
+            return head;
+        int len = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            len++;
+            tail = tail.next;
+        }
+        k = k % len;
+        k = len - k;
+        while (k-- > 0) {
+            ListNode temp = new ListNode(head.val);
+            head = head.next;
+            tail.next = temp;
+            tail = tail.next;
+        }
+
+        return head;
+    }
+}
